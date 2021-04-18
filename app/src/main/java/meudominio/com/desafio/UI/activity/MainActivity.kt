8337ac87e4f96f4e05595e2b4f02b580c5077421
gbrb1 <gbrb1@hotmail.com>
2021-04-18 10:35:01 -0300

    Adicionando layout

--- app/src/main/java/meudominio/com/desafio/UI/activity/MainActivity.kt
+++ app/src/main/java/meudominio/com/desafio/UI/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package meudominio.com.desafio
+package meudominio.com.desafio.UI.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import meudominio.com.desafio.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
