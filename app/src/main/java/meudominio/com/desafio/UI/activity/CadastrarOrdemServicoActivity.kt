d3118ea66f74afb50d2cc99e44b82ee61e5b6e91
gbrb1 <gbrb1@hotmail.com>
2021-04-18 13:19:49 -0300

    Adicionando a tela de cadastro de ordem de serviço

---
+++ app/src/main/java/meudominio/com/desafio/UI/activity/CadastrarOrdemServicoActivity.kt
@@ -0,0 +1,19 @@
+package meudominio.com.desafio.UI.activity
+
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import meudominio.com.desafio.R
+
+
+class CadastrarOrdemServicoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastrar_ordem_servico)
+
+
+    }
+
+
+}
--- app/src/main/java/meudominio/com/desafio/UI/activity/MainActivity.kt
+++ app/src/main/java/meudominio/com/desafio/UI/activity/MainActivity.kt
@@ -1,12 +1,24 @@
 package meudominio.com.desafio.UI.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import meudominio.com.desafio.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sem_ordem.visibility = View.GONE
+
+        adicionar_ordem_servico.setOnClickListener{
+            intent = Intent(applicationContext, CadastrarOrdemServicoActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
